96da2fc1809678dc99495f99614c2366431b7880
maaxizin25 <maaxipb25@gmail.com>
2023-12-31 12:59:09 -0300

    add: viewListCreate

--- announcement/views.py
+++ announcement/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from users.permissions import myCustomPermission
+from announcement.models import Announcement
+from rest_framework import generics
 
 # Create your views here.
+class AnnouncementCreateView(generics.ListCreateAPIView):
+    queryset=Announcement.objects.all()
+    permission_classes=[myCustomPermission]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
