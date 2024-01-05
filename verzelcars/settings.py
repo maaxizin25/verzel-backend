782f98a2d0fb99101a377e15e1d0fc10ed5e9b0a
maaxizin25 <maaxipb25@gmail.com>
2024-01-05 12:27:09 -0300

    fix: quantity-itens-return

--- verzelcars/settings.py
+++ verzelcars/settings.py
@@ -68,7 +68,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
+    "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
